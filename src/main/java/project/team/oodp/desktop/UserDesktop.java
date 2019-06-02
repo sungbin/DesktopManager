afe9c8c8e6ecabe393f7da7309df6b1ab38d59cd
SUJIN <21500394@handong.edu>
2019-06-03 00:23:20 +0900

    arrangement

--- src/main/java/project/team/oodp/desktop/UserDesktop.java
+++ src/main/java/project/team/oodp/desktop/UserDesktop.java
@@ -29,15 +29,14 @@ import project.team.oodp.desktop.login.SuperLogIn;
 import project.team.oodp.desktop.login.SuperLogInFactory;
 
 public class UserDesktop extends Desktop{
-	
-	
+
+
 	public static JPanel whole_panel, center_panel, bottom_panel, side_panel;
 	public static JButton window_btn, paint_btn,clock_btn;
 	JLabel empty1, empty2, empty3, empty4, empty5, empty6;
 	JButton create_folder, delete_folder, create_file, delete_file;
-	
+
 	//수정 
-	//FileManager fm = new FileManager();
 	JButton[] folder_btn = new JButton[30];
 	static int number_of_folder = 0;
 	static ArrayList<String> folder_name = new ArrayList<String>();
@@ -45,33 +44,26 @@ public class UserDesktop extends Desktop{
 	FileMaker makeFileColleague = new FileMaker(mediator);
 	FileDeleter deleteFileColleague = new FileDeleter(mediator);
 
-	
-	
+
+
 	public UserDesktop() throws IOException{
-		
-//		JFrame jf = new JFrame("Welcome to Desktop Manager :)");
-		
+
 		//superlogin에서 current_id 추가해주기 
 		FlyweightFactory factory = new FlyweightFactory();
 		String flyweight = factory.getFlyweight(SuperLogIn.current_id);
 		setTitle(flyweight);
-		
-		
+
+
 		setSize(1000,800);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setResizable(false);
 		setVisible(true);
-//		add(jf);
-		
+
 		setLayout(new BorderLayout());
 		center_panel = new JPanel();
 		center_panel.setBackground(new Color(0, 128, 128));
-//		BufferedImage img = ImageIO.read(new 0File("D:/HGU_2019_1/객체지향/Desktop_images/window.jpg"));
-//		JLabel lbl = new JLabel(new ImageIcon(img));
-//		center_panel.add(lbl);
-		
+
 		side_panel = new JPanel();
-//		side_panel.setLayout(new GridLayout(1,6));
 		side_panel.setSize(100,100);
 		side_panel.setBackground(Color.yellow);
 		create_folder = new JButton("Create Folder");
@@ -82,8 +74,8 @@ public class UserDesktop extends Desktop{
 		side_panel.add(delete_folder);
 		side_panel.add(create_file);
 		side_panel.add(delete_file);
-		
-		
+
+
 		bottom_panel = new JPanel();
 		bottom_panel.setBackground(Color.GRAY);
 		bottom_panel.setSize(100,100);
@@ -96,7 +88,7 @@ public class UserDesktop extends Desktop{
 				new SuperLogInFactory().createLogIn();
 				dispose();
 			}
-			
+
 		});
 		bottom_panel.add(window_btn);
 		empty1 = new JLabel();
@@ -108,111 +100,108 @@ public class UserDesktop extends Desktop{
 
 		bottom_panel.add(create_folder);
 		bottom_panel.add(delete_folder);
-		
+
 		JPanel clock_panel = new JPanel();
 		SuperClock time = new SuperClock();
-	      
-	      clock_panel.add(time);
-	      
+
+		clock_panel.add(time);
+
 		bottom_panel.add(clock_panel);      
-		
-		
-		
+
 		side_panel = new JPanel();
-		
-//		add(side_panel, "We");
+
 		add(center_panel, "Center");
 		add(bottom_panel, "South");
-		
+
 		//수정 
-		
-		
+
+//폴터 생성
 		create_folder.addActionListener(new ActionListener() {
-			
+
 			public void actionPerformed(ActionEvent e) {
 				center_panel.setVisible(false);
 				side_panel.setVisible(false);
 				bottom_panel.setVisible(false);
 				setTitle("Create Folder");
 				setSize(400, 200);
-				
+
 				setLocation(150, 150);
 				setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 				JPanel folder_panel = new JPanel();
 				add(folder_panel);
-				
+
 				makeFileColleague.modifyFile(folder_panel);	
 			}
 
 		});
-		
+
 		//어플리케이션 생성하는 부분 
 		center_panel.setLayout(null);
-		
+
 		ApplicationIcon makePaintIcon = new PaintIcon();
 		ApplicationIcon makeClockIcon = new ClockIcon();
 		makePaintIcon.makeApplicationIcon();
 		makeClockIcon.makeApplicationIcon();
-		
-		
-		
+
+
+
 		//폴더를 생성하는 부분 
 		FolderColor yellow = new YellowFolder();
 		FolderColor red = new RedFolder();
-		Iterator<String> itr = folder_name.iterator();
-		
 
-				if(number_of_folder > 0) {
-					int folder_pointer = 0;
-					int folder_pointer2 = 0;
-					center_panel.setLayout(null);
-					for (int i = 0; i < number_of_folder; i++) {
-						folder_btn[i] = new JButton();
-						if(i%2 == 0) {
-							folder_btn[i].setBackground(yellow.setFolderColor());
-						}else {
-							folder_btn[i].setBackground(red.setFolderColor());
-						}
-						folder_btn[i].setBounds(80+folder_pointer, 100+150*folder_pointer2, 80, 80);
-						center_panel.add(folder_btn[i]);
-						
-						JLabel name = new JLabel(folder_name.get(i));
-						name.setBounds(100+folder_pointer, 150+150*folder_pointer2, 80, 80);
-						center_panel.add(name);
-						
-						folder_pointer2++;
-						
-						if((i+1) % 4 == 0) {
-							folder_pointer += 140;
-							folder_pointer2 = 0;
-						}
-					  }
+
+		if(number_of_folder > 0) {
+			int folder_pointer = 0;
+			int folder_pointer2 = 0;
+			center_panel.setLayout(null);
+			for (int i = 0; i < number_of_folder; i++) {
+				folder_btn[i] = new JButton();
+				if(i%2 == 0) {
+					folder_btn[i].setBackground(yellow.setFolderColor());
+				}else {
+					folder_btn[i].setBackground(red.setFolderColor());
 				}
-				
-				delete_folder.addActionListener(new ActionListener() {
-					
-					public void actionPerformed(ActionEvent e) {
-						center_panel.setVisible(false);
-						side_panel.setVisible(false);
-						bottom_panel.setVisible(false);
-						
-						setTitle("Delete Folder");
-						setSize(400, 200);
-						
-						setLocation(150, 150);
-						setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				folder_btn[i].setBounds(80+folder_pointer, 100+150*folder_pointer2, 80, 80);
+				center_panel.add(folder_btn[i]);
 
-						JPanel folder_panel = new JPanel();
-						add(folder_panel);
-						
+				JLabel name = new JLabel(folder_name.get(i));
+				name.setBounds(100+folder_pointer, 150+150*folder_pointer2, 80, 80);
+				center_panel.add(name);
 
-						setVisible(true);
-						
-						deleteFileColleague.modifyFile(folder_panel);
-					}
+				folder_pointer2++;
 
-				});
+				if((i+1) % 4 == 0) {
+					folder_pointer += 140;
+					folder_pointer2 = 0;
+				}
+			}
+		}
+
+//폴더 삭제 
+		delete_folder.addActionListener(new ActionListener() {
+
+			public void actionPerformed(ActionEvent e) {
+				center_panel.setVisible(false);
+				side_panel.setVisible(false);
+				bottom_panel.setVisible(false);
+
+				setTitle("Delete Folder");
+				setSize(400, 200);
+
+				setLocation(150, 150);
+				setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+				JPanel folder_panel = new JPanel();
+				add(folder_panel);
+
+
+				setVisible(true);
+
+				deleteFileColleague.modifyFile(folder_panel);
+			}
+
+		});
 	}
-	
+
 }
